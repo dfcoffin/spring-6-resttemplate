606fb81fe330a25610f0892d78fdd270261abb4d
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-03-27 00:17:24 -0400

    Adding client classes

---
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClient.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6resttemplate.client;
+
+import guru.springframework.spring6resttemplate.model.BeerDTO;
+import org.springframework.data.domain.Page;
+
+/**
+ * @author Donald F. Coffin, Green Button Alliance, Inc.
+ */
+
+public interface BeerClient {
+
+	Page<BeerDTO> listBeers();
+}
---
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -0,0 +1,15 @@
+package guru.springframework.spring6resttemplate.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Donald F. Coffin, Green Button Alliance, Inc.
+ */
+class BeerClientImplTest {
+
+	@Test
+	void listBeers() {
+	}
+}
