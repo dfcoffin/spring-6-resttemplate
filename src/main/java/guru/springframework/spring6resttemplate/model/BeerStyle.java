02f5593c0cdab614ac6d59be6f6bef7ce6a6082b
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-03-26 23:54:56 -0400

    Initial commit

---
+++ src/main/java/guru/springframework/spring6resttemplate/model/BeerStyle.java
@@ -0,0 +1,8 @@
+package guru.springframework.spring6resttemplate.model;
+
+/**
+ * @author Donald F. Coffin, Green Button Alliance, Inc.
+ */
+public enum BeerStyle {
+	LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
